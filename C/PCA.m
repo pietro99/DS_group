211c1e9ce40888e2717db7f0a9515be023465651
pietro99 <pietro.piccini@hotmail.com>
2023-06-14 13:49:13 +0200

    added labels fetching

--- C/PCA.m
+++ C/PCA.m
@@ -3,25 +3,47 @@ base_folder = 'data';
 inputFolder = 'chest-rays';
 outputFolder = inputFolder+"_processed";
 
-fl = get_file_list(base_folder, outputFolder);
+[fl, labels] = get_file_list(base_folder, outputFolder);
 
 %%
 %get size of images
-example_image = imread(fullfile(fl(1).folder , fl(1).name));
+example_image = imread(fullfile(fl{1}(1).folder , fl{1}(1).name));
 [h, w, d] = size(example_image);
-
-%construct data matrix 
-D = zeros(w*h*d, length(fl));
+len = 0;
 for i = 1:numel(fl)
-    image =imread(fullfile(fl(i).folder , fl(i).name));
-    [h, w, d] = size(image);
-    x = double(reshape(image, w*h*d, 1))/255;
-    D(:, i) = x;
+    len = len + length(fl{i});
 end
+%construct data matrix 
+D = zeros(w*h*d, len);
+L = strings(len, 1);
+for i = 1:numel(fl)
+    label = labels{i};
+    for j = 1:numel(fl{i})
+        image =imread(fullfile(fl{i}(j).folder , fl{i}(j).name));
+        [h, w, d] = size(image);
+        x = double(reshape(image, w*h*d, 1))/255;
+        D(:, i) = x;
+        L(j, 1) = label;
+    end
+end
+
+num_of_rand = 10;
+rand_pixels = randperm(size(D, 1),num_of_rand);
+rand_images = randperm(size(D, 2),num_of_rand);
+
+D_sampled_img =D(:,rand_images);
+D_sampled_pixels =D(rand_pixels,:);
+%PCA, Newdata = calc_mapping(10, D);
+
+%%
+
+Data_matrix = D;
+
+
 %%
 
 %constuct mean vector
-D_means = mean(D, 2);
+D_means = mean(Data_matrix, 2);
 
 %get average image
 average_image =  uint8(reshape(D_means, h, w, d)*255);
@@ -29,11 +51,12 @@ average_image =  uint8(reshape(D_means, h, w, d)*255);
 
 %%
 
-M = D - D_means;
-m = length(M);
-d_length = length(D);
+M = Data_matrix - D_means;
 
-image_reference =  uint8(reshape(D(:,1), h, w, d)*255);
+m = length(M);
+d_length = length(Data_matrix);
+
+image_reference =  uint8(reshape(Data_matrix(:,1), h, w, d)*255);
 image_reference_diff =  uint8(reshape(M(:,1), h, w, d)*255);
 %imshow(image_reference)
 %imshow(image_reference_diff)
@@ -41,22 +64,22 @@ image_reference_diff =  uint8(reshape(M(:,1), h, w, d)*255);
 %%
 
 %compute covariance matrix
-%C =  (1/m) * (M*M');
+C =  (1/m) * (M*M');
 
-G = (1/m) * (M'*M);
+%G = (1/m) * (M'*M);
 
 
 %%
 %compute eigenvalues and eigenvectors
 num_of_eigenvec = 10;
-[Vec, D_val] = eigs(G, num_of_eigenvec);
+[Vec, D_val] = eigs(C, d);
 eig_vals = diag(D_val);
 
 %%
 %compute basis vectors
-%U = Vec;
+U = Vec;
 
-U = (1 ./ sqrt(eig_vals))' .* (M * Vec);
+%U = (1 ./ sqrt(eig_vals))' .* (M * Vec);
 
 %%
 
@@ -86,7 +109,9 @@ end
 %get eigenvectors weights for image
 image_index = 3800;
 
-weights_image = U' * (M(:, image_index)- D_means);
+new_dim = U' * Data_matrix;
+
+weights = new_dim(:,image_index);
 
 %show original image
 imshow(reshape(D(:, image_index), h, w, d));
@@ -113,3 +138,58 @@ for i = 1:length(U(1,:))
     new_image = rescale(new_image,0,1);
 end
 imshow(uint8(reshape(new_image, h, w, d)*255));
+
+
+%%
+function [PCA, Newdata] = calc_mapping(L, data)
+    l=size(L,2);
+
+
+    Index = 1 : size(data,2);
+
+    FirstL = data(:,L);
+
+    RMI = setdiff(Index,L);
+
+    RestMat = data(:,RMI);
+
+    Newdata = [FirstL RestMat];
+
+    Newdata = Newdata - mean(Newdata,1);
+
+    m = size(data,1);
+
+    NyCov = (Newdata' * Newdata(:,1:l))./(m-1); %the natural way to compute it. 
+
+    NyCovA = NyCov(1:l,1:l);
+
+    NyCovB = NyCov((l+1):end,1:l);
+
+    [eigvecA, eigvalA] =eig(NyCovA,'matrix');
+    %computing the indices of the eigen values in decreasing order.
+    [d,ind] = sort(diag(eigvalA),'descend');
+    %sorting eigen values as per the computed indices.
+    eigvalsorted = eigvalA(ind,ind);
+    % similarly sorting the eigen vectors as per the computed indices.
+    eigvecsorted = eigvecA(:,ind);
+
+    U_A = eigvecsorted;
+
+    U_B = NyCovB * U_A * inv(eigvalsorted); 
+
+    PCA_Mat = [U_A; U_B];
+
+
+    PCA = zeros(size(PCA_Mat));
+
+    k = 1;   
+    for a = L
+        PCA(a,:)=PCA_Mat(k,:)/norm(PCA_Mat(k,:));
+        k=k+1;
+    end
+
+    for a = RMI
+        PCA(a,:)=PCA_Mat(k,:)/norm(PCA_Mat(k,:));
+        k=k+1;
+    end
+end
--- C/get_file_list.m
+++ C/get_file_list.m
@@ -1,16 +1,22 @@
-function fileList = get_file_list(base_folder, folder)
+function [fileList, labels] = get_file_list(base_folder, folder)
     % Get a list of all subfolders within the input folder
     subfolders = dir(fullfile(base_folder, folder));
     subfolders = subfolders([subfolders.isdir]);
     subfolders = subfolders(3:end);  % Exclude "." and ".." directories
     
-    fileList = [];
+    fileList = cell(numel(subfolders), 1);
+    labels = cell(numel(subfolders), 1);
+    labeledData = table();
     % Loop through each subfolder
     for i = 1:numel(subfolders)
         subfolderPath = fullfile(base_folder,folder, subfolders(i).name);
         % Get a list of all image files within the subfolder
         imageFiles = dir(fullfile(subfolderPath, '*')); 
+         
         imageFiles = imageFiles(3:end);
-        fileList = [fileList; imageFiles];
+        fileList{i} = imageFiles;
+        labels{i} = subfolders(i).name;
+        
+        %fileList = [fileList; imageFiles];
     end
 end
